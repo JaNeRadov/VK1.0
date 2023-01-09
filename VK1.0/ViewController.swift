3ef3b294ddd935ee980b5359c69df5947611f881
Yuriy Zarubin <temisfucincrazy@icloud.com>
2023-01-09 20:18:07 +0300

    Initial Commit

---
+++ VK1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK1.0
+//
+//  Created by Jane Z. on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
